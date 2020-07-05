2a8cfb7c44c9d31ac17e09c18950a74ec1199129
Mark Mavromatis <markmavromatis@Marks-2020-Macbook-Pro-13.local>
2020-07-05 13:55:21 -0400

    Updated starter code to pass TypeScript compiler check

--- Project/backend/src/lambda/auth/auth0Authorizer.ts
+++ Project/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,20 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
+const cert = ""
+
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// const jwksUrl = '...'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,15 +58,16 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // console.log(jwt)
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return token
 }
 
-function getToken(authHeader: string): string {
+function getToken(authHeader: string): JwtPayload {
   if (!authHeader) throw new Error('No authentication header')
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -73,5 +76,10 @@ function getToken(authHeader: string): string {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  return token
+  return verify(
+    token,           // Token from an HTTP header to validate
+    cert,            // A certificate copied from Auth0 website
+    { algorithms: ['RS256'] } // We need to specify that we use the RS256 algorithm
+  ) as JwtPayload
+  
 }
--- Project/backend/src/lambda/http/getTodos.ts
+++ Project/backend/src/lambda/http/getTodos.ts
@@ -4,4 +4,10 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } f
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log(event)
+  return {
+    statusCode: 200,
+  body: JSON.stringify({
+    someName: "someValue"
+  })}
 }
