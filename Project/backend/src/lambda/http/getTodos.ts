8ee1ebfd3d0722790644e9c14b22614308331dca
Mark Mavromatis <markmavromatis@Marks-2020-Macbook-Pro-13.local>
2020-07-05 14:44:37 -0400

    Implemented GetToDos Lambda function

--- Project/backend/src/lambda/http/getTodos.ts
+++ Project/backend/src/lambda/http/getTodos.ts
@@ -1,13 +1,53 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import {parseUserId} from "../../auth/utils"
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+const todoUserIdIndex = process.env.USER_ID_INDEX
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
-  console.log(event)
+  console.log(JSON.stringify(event))
+
+  // Retrieve User ID
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
+  const userId = parseUserId(jwtToken);
+
+  console.log("User ID: " + userId)
+  
+  // Query DynamoDB for ToDos belonging to this user
+  const result = await docClient.query({
+    TableName : todosTable,
+    IndexName : todoUserIdIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+        ':userId': userId
+    }
+  }).promise()
+
+  // Return results (if there are any). Otherwise return a 404 (resource not found)
+  if (result.Count !== 0) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(result.Items)
+    }
+  }
+
   return {
-    statusCode: 200,
-  body: JSON.stringify({
-    someName: "someValue"
-  })}
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
+
 }
--- Project/client/src/config.ts
+++ Project/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '594625448788'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-euud751t.us.auth0.com',            // Auth0 domain
+  clientId: 'mrTftgFUUIuzRZcSG3i8KK51C5Ley2gY',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
